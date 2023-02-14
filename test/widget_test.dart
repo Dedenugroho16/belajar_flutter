9913f720967793d1f95bd09d6c844b7aa159bc42
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-02-15 01:12:20 +0700

    Belajar tipe data pada bahasa pemrograman dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,32 @@
 void main() {
-  // fungsi main adalah fungsi yang pertama kali dijalankan
-  // void => fungsi yang tidak mengembalikan nilai
+  // tipe data
+  // 1. tipe data string
+  // 2. tipe data boolean
+  // 3. tipe data number => tipe data yang bisa dikasih koma
+  // 4. tipe data object
+  // 5. tipe data double => tipe data yang khusus untuk bilangan desimal
+  // 6. tipe data list 
+  // 7. tipe data dynamic => tipe data yang terdapat pada tipe data list
+  // 8. tipe data map => associative array nya bahasa dart
 
-  // karakteristik dart
-  // 1. Dart adalah bahasa yang statically typed, kita perlu mendefinisikan variable sebelum digunakan
-  // 2. Type Inference => karakteristik ini dapat membuat suatu variable akan mengikuti tipe data dari valuenya. Namun hal tersebut tidak berlaku jika sudah memberi tipe data pada variablenya
-  // 3. String expression => kita dapat menggunakan tanda $ untuk menampilkan suatu variable bertipe apapun ke bentuk string. Hal ini dapat kita sebut sebagai string interpolation
-  // 4. Object oriented programming => dapat berfokus pada paradigma oop yang mana berorientasi pada object
-  var nama = "Dede Nugroho";
-  var umur = 19;
 
-  print("Nama saya adalah $nama, dan umur saya sekarang $umur tahun");
+
+  String name = "dede nugroho";
+  bool isMale = true;
+  num age = -19;
+  double salary = 1000000000;
+  List favColor = ["Merah", "Kuning", "Hijau"];
+  Map kendaraan = { 
+    "Mobil": "NISSAN GTR",
+    "Motor": "NMAX",
+  };
+
+
+  print(name.toUpperCase());
+  print("Umur saya ${age.abs()}"); // age.ceil() membulatkan nilai desimal
+  print(isMale);
+  print(salary);
+  print("Warna fav pertama adalah : ${favColor[0]}");
+  print("Nama motor Dede : ${kendaraan["Motor"]}");
+  print("Nama motor Dede : ${kendaraan["Mobil"]}");
 }
