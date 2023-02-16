acaddacd0a690a22ce7b94e1d4e8d0da88aba158
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-02-16 13:35:58 +0700

    Belajar looping statement(pernyataan perulangan pada bahasa pemrograman dart)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,30 @@
 void main() {
-// materi exceptions(try & catch)
-
-  try {
-    int umur = int.parse('abc');
-    print(umur);
-  } on FormatException {
-    print('Data yang anda masukkan formatnya salah');
-  } catch (error) {
-    print(error); // di dalam catch ini untuk error secara general
+// looping statement
+// 1. looping
+//   for(int i = 0; i<=100; i++) {
+//     i=i+1;
+//     print("Astaghfirullah $i" + "x");
+//   }
+  
+// 2. do while
+//   int i = 1;
+//   do {
+//     print("Astaghfirullah $i"+"x");
+//     i = i + 1;
+//   } while(i<10);
+  
+  List<String> motor = ['Mio', 'Nmax', 'Supra'];
+  
+//   motor.forEach(
+//     (motor) => print("Motor: $motor")
+//   );
+  
+  for(String m in motor) {
+    print("Motor: $m");
   }
+  
+//   for(int i = 0; i<=motor.length-1; i++) {
+//     print("Motor : ${motor[i]}");
+//   }
+  
 }
