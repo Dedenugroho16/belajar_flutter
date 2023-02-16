55443d4acfcaa1648ec40efab4f3509024398dde
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-02-17 00:54:10 +0700

    Belajar collections/array-nya bahasa dart (list, set, map) dan belajar spread collections

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,114 @@
 void main() {
-  String nama = "jono";
-  if(nama=="Dewi") {
-    print("Nama pacar saya adalah $nama");
-  } else if(nama=="Hilyah") {
-    print("$nama adalah teman sekelas saya");
-  } else {
-    print("Saya tidak kenal $nama hahahahahaha");
+  
+  // Collection/array-nya bahasa dart 
+  // 1. List
+  // pengambilan data menggunakan indexing misal motor[0]
+  List motor = ["Vario", "Nmax", "Ninja"];
+  print(motor);
+  print("\n");
+  
+  int i=0;
+  motor.forEach((item) {
+    print("motor ke-${i+1} : ${item}");
+    i++;
+  });
+  
+  print("\n");
+  
+  int x=0;
+  for(String item in motor) {
+    print("motor ke-${x+1} : ${item}");
+    x++;
   }
   
-  String name="jajang";
-  switch(name) {
-    case "jono" : print("Nama dia adalah jono");
-      break;
-    case "dewi" : print("Nama pacar saya adalah $name");
-      break;
-    case "dede" : print("Nama saya adalah $name");
-      break;
-    default : print("Saya tidak kenal $name hahahahahaha");
-      break;
+  print("\n");
+  
+  for(int y=0; y<=motor.length-1; y++) {
+    print("motor ke-${y+1} : ${motor[y]}");
   }
+  print("\n \n");
+  
+  
+  
+  
+  // 2. set
+  // pengambilan data menggunakan atribut .elementAt()
+  Set nilaiJarkom = {9, 7, 6, 8}; // kumpulan data yang di-set di dalam kurung kurawal
+  Set nilaiPPB = {5, 2, 3, 4};
+  // print(nilaiJarkom.elementAt(0));
+  print(nilaiJarkom.union(nilaiPPB));
+  print("\n \n");
+  /*
+      ketika kita menggunakan set, maka kita bisa menjalankan fungsi rumus
+      Operasi Himpunan (Irisan, Gabungan, Selisih dan Komplemen)
+      dalam contoh diatas menggunakan rumus gabungan(union)
+      
+      sedangkan ketika kita menggunakan list, kita tidak bisa melakukan ini
+  */ 
+  
+  
+  
+  
+  
+  // 3. map => array associative nya bahasa pemrograman dart :D
+  // menggunakan key dan value
+  Map nilaiGue = {
+    "matematika" : [5, 2, 3, 4],
+    "fisika" : [9, 7, 6, 8]
+  };
+  print(
+    "nilai mtk gue adalah: ${nilaiGue["matematika"][1]} sedangkan nilai fisika gue adalah: ${nilaiGue["fisika"]}"
+  );
+  print("================================================================= \n\n\n");
+  
+  
+  
+  
+  // Kesimpulan 
+  // list collection biasa seperti array (indexing)
+  // set mirip seperti list yang membedakan hanya penulisannya menggunakan kurung kurawal dan bisa menjalankan operasi himpunan matematika, (indexing)
+  // map seperti associative array yang mempunyai key dan value
+  
+  
+  
+  
+  
+    
+   
+   // Spread collection bisa digunakan di collection list dan juga di collection set
+   // spread collection ini seperti mengcopy nilai array lain ke array yang baru, namun nilainya bisa diedit(hapus, tambah, ubah)
+   List data = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 0];
+   
+   // pengunaan spread collection di list 
+   List copyData = [...data, 5];
+   copyData[4] = null;
+   print(copyData);
+   print("\n\n");
+  
+   Set data1 = {1, 2, 3, 4, 5 , 6, 7, 8, 9, 0};
+   // pengunaan spread collection di set
+   Set copyData1 = {...data1, 5};
+   print(copyData1.elementAt(2));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
