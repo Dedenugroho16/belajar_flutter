40cc89517c397802d4871f631b46dd69651e8562
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-02-20 01:16:33 +0700

    Belajar Class (attribute, constructor,  method, setter, dan getter)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,26 +1,11 @@
+import 'Pakaian.dart';
+
 void main() {
-  // null safety adalah fitur untuk mengetahui adanya error akibat null
-  // ada tiga jenis syntax baru yaitu
-  // ?
-  // !
-  // late
-
-  // fitur null safety pertamakali ada pada sdk versi ke 2.12
-  // ? => variable diperbolehkan null
-  // ?? => untuk menghandle null
-  // ! => kita sudah memastikan dengan sesungguhnya/yakin banget loohhh bahwa data sudah pasti ada dan tidak null
-  // late => untuk memastikan bahwa sebelum diparsing wajib diinisialisasi
-
-  late String nama;
-  
-  // sebelum data diparsing ke fungsi printNama, maka data wajib diisi dulu value nya, 
-  //misal ngga diisi maka akan error, hal itu terjadi karena penggunaan sintaks "late""
-  nama = "Wednesday"; 
-
-  printNama(nama);
-
-}
-
-String? printNama(String parameterNama) {
-  print(parameterNama);
+  // var pakaian1 = Pakaian("Jaket", "Biru"); // positional arguments (posisi value nya harus sesuai dengan argument)
+  // var pakaian1 = Pakaian(warnanya: "Biru", jenisnya: "Jaket"); // named arguments (posisi nya bebas tapi harus sesuai dengan nama argument)
+  var pakaian1 = Pakaian(warna: "Biru", jenis: "Jaket", ukuran: "XXL"); // direct named arguments constructor => argument yang diparsing ke class Pakaian akan otomatis diinisialisasi ke attribute class tersebut
+  print("${pakaian1.jenis} warna - ${pakaian1.warna} - ukuran - ${pakaian1.getUkuran}");
+  pakaian1.setUkuran = "XL";
+  print("Ganti Ukuran");
+  print("${pakaian1.jenis} warna - ${pakaian1.warna} - ukuran - ${pakaian1.getUkuran}");
 }
