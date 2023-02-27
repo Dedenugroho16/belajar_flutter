1d6f9c720718f1d7e32fc30291a28f4215b50ff4
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-02-27 21:59:09 +0700

    Widget dasar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,12 +7,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("Hello world!")),
+        appBar: AppBar(
+          backgroundColor: Colors.red[900],
+          title: Text("Aku adalah manusia super"),
+          centerTitle: true,
+        ),
+        body: Center(
+          // Image Provider:
+          // 1. AssetImage -> gambar yang ada pada folder project, dan perlu didaftarkan di pubspec.yaml
+          // 2. NetworkImage -> ambil dari internet(url)
+          // Jarang digunakan -> pada kasus tertentu
+          // 3. FileImage
+          // 4. MemoryImage
+
+
+
+          // child: Image(
+          //   image: AssetImage("assets/dede.jpg"),
+          // ),
+
+          // cara singkat
+          child: Image.asset("assets/dede.jpg"),
+
+        )
       ),
     );
   }
