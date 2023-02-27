421b862e1472a3c4dc1945792a8bc3d478ba89d8
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-02-28 04:27:32 +0700

    Widget Builder

--- lib/main.dart
+++ lib/main.dart
@@ -8,44 +8,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
-  // List<KotakWarna> allItems = List.generate(
-  //   10, 
-  //   (index) => KotakWarna(
-  //     text: "Kotak - ${index+1}", 
-  //     warna: Color.fromARGB(
-  //       255, 
-  //       Random().nextInt(256), 
-  //       Random().nextInt(256), 
-  //       Random().nextInt(256)
-  //     )
-  //   )
-  // );
-
-  List<Map<String, dynamic>> data = List.generate(
-    10, 
-    (index) => {
-      "text": "Kotak - ${index+1}",
-      "color": Color.fromARGB(
-        255, 
-        Random().nextInt(256), 
-        Random().nextInt(256), 
-        Random().nextInt(256)
-      )
-    }
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Extract Widget"),),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: data.map((e) => KotakWarna(text: e["text"], warna: e["color"])).toList(),
+          appBar: AppBar(
+            title: Text("Extract Widget"),
           ),
-        ),
+          body: GridView.builder(
+              padding: EdgeInsets.all(5),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 5, crossAxisSpacing: 5), 
+              itemBuilder: (context, index) => Container(
+                color: Color.fromARGB(255, Random().nextInt(256), Random().nextInt(256), Random().nextInt(256)),
+              ),
+          )
       ),
     );
   }
