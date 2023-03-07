78b71b7d41b77e1a915aec7c5e94990fc21bf2e7
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-03-07 22:21:56 +0700

    Textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use
+
+// vid ke 4 14:00
 
 import 'package:flutter/material.dart';
 
@@ -17,49 +19,91 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
   });
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  bool isHidden = true;
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passwordC = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("SnackBar"),
+        title: Text(
+          "Text Field",
+        ),
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                  "Update Berhasil",
-                  style: TextStyle(
-                    color: Colors.blue
-                  ),
-                ),
-                action: SnackBarAction(
-                  label: "Cancel", 
-                  onPressed: (){},
-                  textColor: Colors.red,
-                ),
-                backgroundColor: Colors.amber,
-                duration: Duration(seconds: 3),
-                margin: EdgeInsets.all(20),
-                behavior: SnackBarBehavior.floating,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)
-                ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: emailC,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              labelText: "email",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15)
+              ),
+              prefixIcon: Icon(Icons.email),
+            ),
+          ),
+          SizedBox(height: 20,),
+          TextField(
+            controller: passwordC,
+            autocorrect: false,
+            obscureText: isHidden,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              labelText: "password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15)
+              ),
+              prefixIcon: Icon(Icons.vpn_key),
+              suffix: IconButton(
+                onPressed: (){
+                  if(isHidden==true) {
+                    isHidden = false;
+                  } else {
+                    isHidden = true;
+                  }
+                  setState(() {});
+                }, 
+                icon: Icon(Icons.remove_red_eye)
+              ),
+            ),
+          ),
+          SizedBox(height: 50,),
+          ElevatedButton(
+            onPressed: (){
+              print("LOGIN DENGAN EMAIL: '${emailC.text}', PASSWORD: '${passwordC.text}'");
+            }, 
+            // ignore: sort_child_properties_last
+            child: Text(
+              "Login",
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.green,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15)
               )
-            );
-          }, 
-          child: Text(
-            "SHOW SNACKBAR",
+            ),
           )
-        )
-      ),
+        ],
+      )
     );
   }
 }
